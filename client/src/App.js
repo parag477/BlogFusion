68380ed822d6c5aff0320965fae839cef1a15b6c
Parag <paragagrawal477@gmail.com>
2023-06-14 22:24:20 +0530

    initial commit 🎉

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">BlogFusion</a>
+        <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
